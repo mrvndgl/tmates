b87474ad57d717382e7d798cb9dfe380ff73adc9
mrvndgl <johnmarvindigal@gmail.com>
2025-07-02 20:33:07 +0800

    Initial changes

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <img src="/logo.png" className="logo" alt="Logo" />
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
